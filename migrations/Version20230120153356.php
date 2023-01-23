5c7e6d10b7cc9bc25bfe2fa9cb4ed08ad744aec8
dawidbros1 <dawidbros1@wp.pl>
2023-01-23 10:34:18 +0100

    delete records from table

--- migrations/Version20230120153356.php
+++ migrations/Version20230120153356.php
@@ -20,12 +20,12 @@ final class Version20230120153356 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE roles (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql("INSERT INTO roles (name, description) VALUES ('ROLE_STUDENT','Uczeń'), ('ROLE_TEACHER','Nauczyciel'), ('ROLE_ADMIN','Administrator')");
+        // $this->addSql("INSERT INTO roles (name, description) VALUES ('ROLE_STUDENT','Uczeń'), ('ROLE_TEACHER','Nauczyciel'), ('ROLE_ADMIN','Administrator')");
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql("DELETE FROM roles");
+        // $this->addSql("DELETE FROM roles");
         $this->addSql('DROP TABLE roles');
     }
 }
