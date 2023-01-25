fa8a645547826866873692231fbea0d6ea59c354
dawidbros1 <dawidbros1@wp.pl>
2023-01-25 17:18:56 +0100

    change in database as migrations

--- migrations/Version20230124105935.php
+++ migrations/Version20230124105935.php
@@ -10,20 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230120153356 extends AbstractMigration
+final class Version20230124105935 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add table roles with data';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE roles (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD pesel VARCHAR(11) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE roles');
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP pesel');
     }
 }
