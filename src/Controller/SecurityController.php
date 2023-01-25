c14d7ad87567dbf7ca76ee9bc2af8fdc29f652cc
dawidbros1 <dawidbros1@wp.pl>
2023-01-25 17:24:03 +0100

    add IsGranted()

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class SecurityController extends AbstractController
 {
     /**
+     * @IsGranted("IS_ANONYMOUS")
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -27,6 +29,7 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @IsGranted("ROLE_USER")
      * @Route("/logout", name="app_logout")
      */
     public function logout(): void
