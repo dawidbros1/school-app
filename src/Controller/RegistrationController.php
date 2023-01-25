7c039cb7b615327d9386fa2504398a3e57383146
dawidbros1 <dawidbros1@wp.pl>
2023-01-25 17:41:45 +0100

    add method initRegister() which create account and which wait for completion register by user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Roles;
+use App\Form\InitRegistrationFormType;
 use App\Form\RegistrationFormType;
+use App\Service\EmailGenerator;
+use App\Service\UserCodeGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
@@ -11,39 +15,59 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_register")
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/register/init/{role_name}", name="app_register_init")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function initRegister(Request $request, EntityManagerInterface $entityManager, EmailGenerator $emailGenerator, UserCodeGenerator $codeGenerator): Response
     {
+        // TODO: Jeżeli użytkownik ma rolę owner to wymagaj rangi ROLE_OWNER
+
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user, []);
+        $role = $entityManager->getRepository(Roles::class)->findOneBy(['name' => $request->get('role_name')]);
+
+        if ($role == null) {
+            dump("Rola nie istnieje");
+            exit();
+        }
+
+        $form = $this->createForm(InitRegistrationFormType::class, $user, []);
+
+        $form->add('submit', SubmitType::class, [
+            'label' => "Dodaj konto",
+            'attr' => [
+                'class' => "btn btn-primary w-100 fw-bold",
+            ],
+        ]);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid() && $this->passwordsAreIdentical($form)) {
-            $user->setRoles([$form->get('role')->getData()->getName()]);
-
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles([$role->getName()]);
+            $user->setEmail($emailGenerator->generate($user));
+            $user->setCode($codeGenerator->generate($user));
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $this->addFlash('success', "Konto zostało założone, możesz się na nie zalogować.");
+            $this->addFlash('success', "Konto zostało zainicjowane, oczekuje na dokończenie rejestracji.");
 
-            return $this->redirectToRoute('app_login');
+            if ($referer = $request->headers->get('referer')) {
+                return $this->redirect($referer);
+            } else {
+                return $this->redirectToRoute('app_dashboard');
+            }
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/initRegistration.html.twig', [
             'form' => $form->createView(),
+            'role' => $role
         ]);
     }
 
