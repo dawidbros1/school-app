aba93ba86f9fc3bec72760d54bd0afd25ed76afd
dawidbros1 <dawidbros1@wp.pl>
2023-01-25 17:29:36 +0100

    add form types

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,17 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Roles;
 use App\Entity\User;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -24,44 +18,6 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class, [
-                'label' => "Imię",
-                'attr' => [
-                    'placeholder' => "Wprowadź imię",
-                    'class' => "form-control"
-                ],
-            ])
-            ->add('lastName', TextType::class, [
-                'label' => "Nazwisko",
-                'attr' => [
-                    'placeholder' => "Wprowadź nazwisko",
-                    'class' => "form-control"
-                ],
-            ])
-            ->add('email', EmailType::class, [
-                'label' => "Adres email",
-                'attr' => [
-                    'placeholder' => "Wprowadź adres email",
-                    'class' => "form-control"
-                ],
-            ])
-            ->add('role', EntityType::class, [
-                'mapped' => false,
-                'class' => Roles::class,
-                'choice_label' => 'description',
-                'choice_value' => 'name',
-                'placeholder' => 'Wybierz rolę',
-                'label' => 'Rola',
-                'required' => true,
-                'attr' => [
-                    'class' => "form-control"
-                ],
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('r')
-                        ->where('r.name != :admin')
-                        ->setParameter('admin', 'ROLE_ADMIN');
-                },
-            ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'label' => "Hasło",
