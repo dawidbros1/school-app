ad4eb94b5468857785965b87d6503f9bfa1182e3
dawidbros1 <dawidbros1@wp.pl>
2023-02-02 13:28:01 +0100

    add UserManager

---
+++ src/Service/UserManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\UserType\Admin;
+use App\Entity\UserType\Student;
+use App\Entity\UserType\Teacher;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserManager
+{
+   private $em;
+
+   public function __construct(EntityManagerInterface $entityManager)
+   {
+      $this->em = $entityManager;
+   }
+
+   public function getEntity(string $userType)
+   {
+      switch ($userType) {
+         case 'student':
+            return new Student();
+         case 'teacher':
+            return new Teacher();
+         case 'admin':
+            return new Admin();
+      }
+   }
+
+   public function getRepository(string $userType)
+   {
+      switch ($userType) {
+         case 'student':
+            return $this->em->getRepository(Student::class);
+         case 'teacher':
+            return $this->em->getRepository(Teacher::class);
+         case 'admin':
+            return $this->em->getRepository(Admin::class);
+      }
+   }
+}
