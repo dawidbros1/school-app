445f2b9214ecef14074982895f021de0cb10b5be
dawidbros1 <dawidbros1@wp.pl>
2023-02-03 16:24:59 +0100

    add methods to interface

--- src/Interfaces/CustomUserInterface.php
+++ src/Interfaces/CustomUserInterface.php
@@ -7,10 +7,18 @@ use Symfony\Component\Security\Core\User\UserInterface;
 interface CustomUserInterface extends UserInterface
 {
    public function getEmail();
+   public function setEmail(string $email);
 
    public function getFirstName();
+   public function setFirstName(string $firstName);
 
    public function getLastName();
+   public function setLastName(string $lastName);
 
    public function getPesel();
+   public function setPesel(string $pesel);
+
+   public function setPassword(?string $password);
+
+   public static function getRole();
 }
