6c5dee7ad86904f5f35f4eb2f7b39fc32c0d9f1d
dawidbros1 <dawidbros1@wp.pl>
2023-02-07 15:11:42 +0100

    add initSchoolClassStatuses

--- src/Command/InitDatabaseCommand.php
+++ src/Command/InitDatabaseCommand.php
@@ -3,12 +3,14 @@
 namespace App\Command;
 
 use App\DataFixtures\Factory\UserFactory;
+use App\Entity\SchoolClass\SchoolClassStatus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+# php bin/console db:init
 class InitDatabaseCommand extends Command
 {
     protected static $defaultName = 'db:init';
@@ -28,20 +30,32 @@ class InitDatabaseCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $this->truncateUserTable();
-        $this->userFactory->create([
-            'email' => "owner@wp.pl",
-            'roles' => 'owner',
-            'password' => "owner"
-        ], true);
+        $this->initUsers();
+        $this->initSchoolClassStatuses();
 
         $io->success('OK');
         return Command::SUCCESS;
     }
 
-    private function truncateUserTable()
+    private function initUsers()
     {
         $this->entityManager->createQuery('DELETE FROM App\Entity\UserType\Owner')->execute();
         $this->entityManager->getConnection()->exec("ALTER TABLE owner AUTO_INCREMENT = 1");
+
+        $this->userFactory->create([
+            'email' => "owner@wp.pl",
+            'roles' => 'owner',
+            'password' => "owner"
+        ], true);
+    }
+
+    private function initSchoolClassStatuses()
+    {
+        $this->entityManager->createQuery('DELETE FROM App\Entity\SchoolClass\SchoolClassStatus')->execute();
+        $this->entityManager->getConnection()->exec("ALTER TABLE school_class_status AUTO_INCREMENT = 1");
+
+        $repository = $this->entityManager->getRepository(SchoolClassStatus::class);
+        $repository->add((new SchoolClassStatus())->setName(SchoolClassStatus::ACTIVE));
+        $repository->add((new SchoolClassStatus())->setName(SchoolClassStatus::ARCHIVED), true);
     }
 }
