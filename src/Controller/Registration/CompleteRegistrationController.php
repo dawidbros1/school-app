e0528b37045566733c022ef4448b89fd58d8e2ac
dawidbros1 <dawidbros1@wp.pl>
2023-02-08 15:13:43 +0100

    separate into two controllers

--- src/Controller/Registration/CompleteRegistrationController.php
+++ src/Controller/Registration/CompleteRegistrationController.php
@@ -1,26 +1,18 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Registration;
 
 use App\Form\CompleteRegistrationFormType;
-use App\Form\RegistrationFormType;
-use App\Service\EmailGenerator;
 use App\Service\FormPasswordManager;
-use App\Service\UserCodeGenerator;
 use App\Service\UserManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Component\Finder\Exception\AccessDeniedException;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
-class RegistrationController extends AbstractController
+class CompleteRegistrationController extends AbstractController
 {
     private $em;
     private $userManager;
@@ -29,55 +21,6 @@ class RegistrationController extends AbstractController
         $this->em = $entityManager;
         $this->userManager = $userManager;
     }
-
-    /**
-     * @IsGranted("ROLE_ADMIN")
-     * @Route("/register/{UserType}", name="app_register")
-     */
-    public function register(Request $request, EmailGenerator $emailGenerator, UserCodeGenerator $codeGenerator, AuthorizationCheckerInterface $authorizationChecker): Response
-    {
-        $userType = $request->get('UserType');
-
-        if ($userType == "admin") {
-            if (!$authorizationChecker->isGranted('ROLE_OWNER')) {
-                throw new AccessDeniedException('Nie masz uprawnień do tworzenia użytkownika typu "admin".');
-            }
-        }
-
-        $user = $this->userManager->getEntity($userType);
-        $form = $this->createForm(RegistrationFormType::class, $user, []);
-
-        $form->add('submit', SubmitType::class, [
-            'label' => "Dodaj konto",
-            'attr' => [
-                'class' => "btn btn-primary w-100 fw-bold",
-            ],
-        ]);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setEmail($emailGenerator->generate($user));
-            $user->setCode($codeGenerator->generate($user));
-
-            $this->em->persist($user);
-            $this->em->flush();
-
-            $this->addFlash('success', "Konto zostało zainicjowane, oczekuje na dokończenie rejestracji przez użytkownika.");
-
-            if ($referer = $request->headers->get('referer')) {
-                return $this->redirect($referer);
-            } else {
-                return $this->redirectToRoute('app_dashboard');
-            }
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'form' => $form->createView(),
-            'role' => $user->getRole()->getDescription()
-        ]);
-    }
-
     /**
      * @IsGranted("IS_ANONYMOUS")
      * @Route("/complete/register/{UserType}", name="app_complete_register")
@@ -106,7 +49,7 @@ class RegistrationController extends AbstractController
                     return $this->redirectToRoute('app_login', ['email' => $user->getEmail()]);
                 }
 
-                return $this->render('registration/completeRegister.html.twig', [
+                return $this->render('registration/complete/register.html.twig', [
                     'form' => $form->createView(),
                     'user' => $user
                 ]);
---
+++ src/Controller/Registration/RegistrationController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Registration;
+
+use App\Form\RegistrationFormType;
+use App\Service\EmailGenerator;
+use App\Service\FormBuilder;
+use App\Service\UserCodeGenerator;
+use App\Service\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $em;
+    private $userManager;
+    public function __construct(EntityManagerInterface $entityManager, UserManager $userManager)
+    {
+        $this->em = $entityManager;
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/register/{UserType}", name="app_register")
+     */
+    public function register(Request $request, EmailGenerator $emailGenerator, UserCodeGenerator $codeGenerator, AuthorizationCheckerInterface $authorizationChecker, FormBuilder $builder): Response
+    {
+        $userType = $request->get('UserType');
+
+        if ($userType == "admin") {
+            if (!$authorizationChecker->isGranted('ROLE_OWNER')) {
+                throw new AccessDeniedException('Nie masz uprawnień do tworzenia użytkownika typu "admin".');
+            }
+        }
+
+        $user = $this->userManager->getEntity($userType);
+        $form = $this->createForm(RegistrationFormType::class, $user, []);
+        $template = "base";
+
+        if ($userType == "student") {
+            $template = $userType;
+            $builder->addClassList();
+        }
+
+        $builder->addButton("Dodaj konto")->build($form);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setEmail($emailGenerator->generate($user));
+            $user->setCode($codeGenerator->generate($user));
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('success', "Konto zostało zainicjowane, oczekuje na dokończenie rejestracji przez użytkownika.");
+
+            if ($referer = $request->headers->get('referer')) {
+                return $this->redirect($referer);
+            } else {
+                return $this->redirectToRoute('app_dashboard');
+            }
+        }
+
+        return $this->render('registration/init/' . $template . '.html.twig', [
+            'form' => $form->createView(),
+            'role' => $user->getRole()->getDescription()
+        ]);
+    }
+}
