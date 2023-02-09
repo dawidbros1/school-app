994fb5e6322fe9755a69fe25e8306909faa017c5
dawidbros1 <dawidbros1@wp.pl>
2023-02-09 17:35:09 +0100

    update in remove teachers

--- src/Command/Clear/UserClearCommand.php
+++ src/Command/Clear/UserClearCommand.php
@@ -2,17 +2,19 @@
 
 namespace App\Command\Clear;
 
+use App\Entity\SchoolClass\SchoolClass;
+use App\Entity\UserType\Teacher;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-# php bin/console db:clear:users
+# php bin/console db:clear:user
 
 class UserClearCommand extends Command
 {
-   protected static $defaultName = 'db:clear:users';
+   protected static $defaultName = 'db:clear:user';
    protected static $defaultDescription = 'The command remove users from database [ exclude owner ]';
    private $em;
 
@@ -27,8 +29,8 @@ class UserClearCommand extends Command
    {
       $io = new SymfonyStyle($input, $output);
 
+      $this->clearTeacherTable(); // [ class <--> teacher ]
       $this->truncateUserTable('Admin', 'admin');
-      $this->truncateUserTable('Teacher', 'teacher');
       $this->truncateUserTable('Student', 'student');
 
       $io->success('users cleared');
@@ -40,5 +42,17 @@ class UserClearCommand extends Command
    {
       $this->em->createQuery("DELETE FROM App\Entity\UserType\\$userType")->execute();
       $this->em->getConnection()->exec("ALTER TABLE $table AUTO_INCREMENT = 1");
+   }
+
+   private function clearTeacherTable()
+   {
+      if ($teachers = $this->em->getRepository(Teacher::class)->findAll()) {
+         foreach ($teachers as $teacher) {
+            $this->em->remove($teacher);
+         }
+         $this->em->flush();
+      }
+
+      $this->em->createQuery("DELETE FROM App\Entity\UserType\Teacher")->execute();
    }
 }
