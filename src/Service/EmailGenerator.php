7b8dd4dd6de868e999f3881b91818027a6420dab
dawidbros1 <dawidbros1@wp.pl>
2023-02-15 12:59:11 +0100

    change range of rand()

--- src/Service/EmailGenerator.php
+++ src/Service/EmailGenerator.php
@@ -17,7 +17,7 @@ class EmailGenerator
          if ($user->getEmail() != null) {
             $index = preg_replace("/[^0-9]/", "", $user->getEmail());
          } else {
-            $index = rand(100, 999); // IN FUTURE USER COUNT + 1000
+            $index = rand(10000, 99999); // IN FUTURE USER COUNT + 10000
          }
 
          $email = strtolower(substr($lastName, 0, 1) . substr($firstName, 0, 1) . $index);
