ff09c54bd69fa1ab97fc6bbd6b97e9aa54ffd85b
dawidbros1 <dawidbros1@wp.pl>
2023-02-22 08:52:19 +0100

    move method fill schedule to another controller

--- src/Controller/Schedule/ScheduleController.php
+++ src/Controller/Schedule/ScheduleController.php
@@ -29,65 +29,6 @@ class ScheduleController extends AbstractController
       $this->em = $entityManager;
    }
 
-   /**
-    * @IsGranted("ROLE_ADMIN")
-    * @Route("/fill/{class_id}/{day}", name="app_schedule_fill")
-    * @Method("POST")
-    */
-   public function fill(Request $request, FormErrors $formErrors)
-   {
-      $day = $request->get('day');
-      $class = $this->getClass($request->get('class_id'));
-
-      $form = $this->createForm(InitializerFormType::class, null, [
-         'action' => $this->generateUrl("app_schedule_fill", ['day' => $day, 'class_id' => $class->getId()])
-      ]);
-
-      $form->handleRequest($request);
-
-      if ($form->isSubmitted() && $form->isValid()) {
-         $min = $form->get("from")->getData();
-         $max = $form->get("to")->getData();
-
-         $dates = [];
-         $scheduleTemplateRepository = $this->em->getRepository(ScheduleTemplate::class);
-         $template = $scheduleTemplateRepository->findBy(['day' => $day, 'class' => $class]);
-
-         while ($max >= $min) {
-            if (ucfirst($day) == $min->format("l")) {
-               $dates[] = clone ($min);
-            }
-
-            $min->modify("+1 day");
-         }
-
-         if (!empty($dates)) {
-            $this->em->getRepository(Schedule::class)->removeIn($class, $dates);
-         }
-
-         foreach ($dates as $date) {
-            foreach ($template as $item) {
-               $object = new Schedule();
-               $object->setClass($item->getClass());
-               $object->setClassTime($item->getClassTime());
-               $object->setTeacher($item->getTeacher());
-               $object->setSubject($item->getSubject());
-               $object->setPlace($item->getPlace());
-               $object->setDate($date);
-
-               $this->em->persist($object);
-            }
-         }
-
-         $this->em->flush();
-         $this->addFlash('success', "Harmonogram został utworzony");
-      } else {
-         $formErrors->set($form);
-      }
-
-      return $this->redirectToRoute('app_scheduleTemplate_show', ['day' => $day, 'class_id' => $class->getId()]);
-   }
-
    private function getClass($id)
    {
       $class = $this->em->getRepository(SchoolClass::class)->findOneBy(['id' => $id]);
