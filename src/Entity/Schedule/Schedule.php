67c4836807e6681c0a737f98b72b3917e1872524
dawidbros1 <dawidbros1@wp.pl>
2023-02-24 12:57:01 +0100

    add method canManage

--- src/Entity/Schedule/Schedule.php
+++ src/Entity/Schedule/Schedule.php
@@ -8,6 +8,11 @@ class Schedule
 {
    private $lessons = [];
 
+   public function __construct(array $lessons = [])
+   {
+      $this->lessons = $lessons;
+   }
+
    public function getLessons(): array
    {
       return $this->lessons;
@@ -16,5 +21,17 @@ class Schedule
    public function addLesson(Lesson $lesson)
    {
       $this->lessons[] = $lesson;
+   }
+
+   public function canManage()
+   {
+      // assumption: all lessons has this same date
+      if (!empty($this->lessons)) {
+         $date = $this->lessons[0]->getDate()->format("Y-m-d");
+         $today = (new \DateTime('now'))->format("Y-m-d");
+         return (bool) ($date >= $today);
+      }
+
+      return false;
    }
 }
