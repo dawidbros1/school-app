a492bd621dcba7bcef8f4ce8449c94016c18e259
dawidbros1 <dawidbros1@wp.pl>
2023-03-01 11:45:41 +0100

    add ScheduleSharedCode

---
+++ src/Service/Shared/ScheduleSharedCode.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Service\Shared;
+
+use App\Entity\Lesson\Lesson;
+use App\Entity\Schedule\Schedule;
+use App\Entity\SchoolClass\SchoolClass;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ScheduleSharedCode
+{
+   private $em;
+   public function __construct(EntityManagerInterface $em)
+   {
+      $this->em = $em;
+   }
+
+   public function getData(SchoolClass $class, \DateTime $date)
+   {
+      $dates = $this->getWeek($date->format("Y-m-d"));
+      $lessons = $this->em->getRepository(Lesson::class)->getIn($class, $dates);
+
+      $nextDate = clone $date->modify("+7 days");
+      $prevDate = clone $date->modify("-14 days");
+
+      $schedules = [];
+
+      foreach ($dates as $date) {
+         $schedules[] = new Schedule([], $date);
+      }
+
+      foreach ($lessons as $lesson) {
+         $N = $lesson->getDate()->format('N') - 1;
+         $schedules[$N]->addLesson($lesson);
+      }
+
+      return [$schedules, $prevDate, $nextDate];
+   }
+
+   private function getWeek($date)
+   {
+      $date = new \DateTime($date);
+      $dates = [];
+
+      while ($date->format("N") != 1) {
+         $date->modify("-1 day");
+      }
+
+      while ($date->format("N") <= 5) {
+         $dates[] = clone ($date);
+         $date->modify("+1 day");
+      }
+
+      return $dates;
+   }
+}
