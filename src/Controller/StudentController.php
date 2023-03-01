78f6668fb5906830dabd9c4c1e2562b29baeb82f
dawidbros1 <dawidbros1@wp.pl>
2023-03-01 11:46:36 +0100

    add StudentController()

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lesson\LessonStatus;
+use App\Entity\Lesson\LessonTime;
+use App\Service\Shared\ScheduleSharedCode;
+use App\Service\User\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @IsGranted("ROLE_STUDENT")
+ * @Route("/student")
+ */
+class StudentController extends AbstractController
+{
+   private $em;
+   public function __construct(EntityManagerInterface $entityManager)
+   {
+      $this->em = $entityManager;
+   }
+
+   /**
+    * @Route("/schedule", name = "app_student_schedule")
+    */
+   public function schedule(Request $request, UserManager $userManager, ScheduleSharedCode $code)
+   {
+      $class = $userManager->getUser()->getClass();
+      $date = new \DateTime($request->get('date', 'now'));
+      [$schedules, $prevDate, $nextDate] = $code->getData($class, $date);
+
+      return $this->render('schedule/show.html.twig', [
+         'class' => $class,
+         'schedules' => $schedules,
+         'lessonTimes' => $this->em->getRepository(LessonTime::class)->findAll(),
+         'lessonStatuses' => $this->em->getRepository(LessonStatus::class)->findAll(),
+         'nextPage' => $this->generateUrl("app_student_schedule", ['date' => $nextDate->format("Y-m-d")]),
+         'prevPage' => $this->generateUrl("app_student_schedule", ['date' => $prevDate->format("Y-m-d")]),
+         'back' => $this->generateUrl("app_dashboard"),
+         'backButtonText' => "Powrót do Dashboard"
+      ]);
+   }
+}
